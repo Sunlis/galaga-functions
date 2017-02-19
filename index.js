436b21d98270e4230c596d92206bef49ba84dab6
Sean Dohring <seandohring@gmail.com>
2017-02-19 01:13:56 -0800

    Refactor

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ exports.lookup = function(req, res) {
     handleLookup(req, res, db);
   } catch (e) {
     console.error(JSON.stringify(e));
-    res.status(500).send(JSON.stringify({
+    jsonResponse(res, 500, {
       'error': '/shrug'
-    }));
+    });
   }
   db.end();
 };
@@ -30,65 +30,71 @@ exports.detail = function(req, res) {
     handleDetail(req, res, db);
   } catch (e) {
     console.error(JSON.stringify(e));
-    res.status(500).send(JSON.stringify({
+    jsonResponse(res, 500, {
       'error': '/shrug'
-    }));
+    });
   }
   db.end();
 };
 
 var handleDetail = function(req, res, db) {
-  if (!req.query.id) {
-    res.status(400).send(JSON.stringify({
-      'error': 'Must provide `id` query param for detail'
-    }));
+  if (!checkParams(req, res, ['id'])) {
     return;
   }
   db.query(
     'SELECT * FROM `systems`.`system_detail` WHERE `id`=?',
     [sanitize(req.query.id)],
     function(error, results, fields) {
-      if (error) {
-        console.error(error);
-        res.status(500).send(JSON.stringify({
-          'error': 'error querying db (see logs)'
-        }));
-      } else {
-        res.status(200).send(JSON.stringify(results));
+      if (!checkError(res, error)) {
+        jsonResponse(res, 200, results);
       }
     });
 };
 
 var handleLookup = function(req, res, db) {
-  if (!req.query.name) {
-    res.status(400).send(JSON.stringify({
-      'error': 'Must provide `name` query param for lookup'
-    }));
+  if (!checkParams(req, res, ['name'])) {
     return;
   }
-  console.log('requested ' + req.query.name);
   db.query('SELECT * FROM `systems`.`systems` WHERE `name` LIKE ? LIMIT 20',
     ['%' + sanitize(req.query.name) + '%'],
     function(error, results, fields) {
-      console.log('results: ' + JSON.stringify(results));
-      if (error) {
-        console.error(error);
-        res.status(500).send(JSON.stringify({
-          'error': 'error querying db (see logs)'
-        }));
-      } else {
-        var mapped = results.map(function(item) {
-          var obj = {};
-          for (var key in item) {
-            obj[key] = item[key];
-          }
-          return obj;
-        });
-        res.status(200).send(JSON.stringify(mapped));
+      if (!checkError(res, error)) {
+        jsonResponse(res, 200, results);
       }
     });
 };
 
 var sanitize = function(txt) {
   return txt.replace(/[^a-zA-Z0-9\-'\s]/g, '');
+};
+
+var jsonResponse = function(res, status, obj) {
+  res.status(status).send(JSON.stringify(obj));
+};
+
+var checkParams = function(req, res, props) {
+  var missing = [];
+  for (var prop in props) {
+    if (!req.query[prop]) {
+      missing.push(prop);
+    }
+  }
+  if (missing) {
+    jsonResponse(res, 400, {
+      'error': 'Missing required query param(s): ' + missing.join(', ')
+    });
+    return false;
+  }
+  return true;
+};
+
+var checkError = function(res, error) {
+  if (error) {
+    console.error(error);
+    jsonResponse(res, 500, {
+      'error': 'error querying db (see logs)'
+    });
+    return true;
+  }
+  return false;
 };
